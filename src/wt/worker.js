92a92d62b39f9dfb09a649f76f8e7e59160cc565
Pavel <pashtet.net5@gmail.com>
2024-10-07 21:55:24 +0300

    refactor:changed function sendResult in wt/worker.js

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,20 +1,21 @@
 // n should be received from main thread
-import { parentPort } from 'worker_threads';
+import { parentPort} from 'worker_threads';
 
-const nthFibonacci = (n) => {
-    if (n < 2) return n;
-    return nthFibonacci(n - 1) + nthFibonacci(n - 2);
-};
+const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-if (parentPort) {
-    parentPort.on('message', (n) => {
-        try {
-            const result = nthFibonacci(n);
-            parentPort.postMessage({ status: 'resolved', data: result });
-        } catch (error) {
-            parentPort.postMessage({ status: 'error', data: null });
-        }
-    });
+const sendResult=()=>{
+ 
+ if (parentPort) {
+  parentPort.on('message', (n) => {
+      try {
+          const result = nthFibonacci(n);
+          parentPort.postMessage({ status: 'resolved', data: result });
+      } catch (error) {
+          parentPort.postMessage({ status: 'error', data: null });
+      }
+  });
 } else {
-    console.error("This script must be run as a worker thread.");
+  console.error("This script must be run as a worker thread.");
 }
+}
+sendResult()
